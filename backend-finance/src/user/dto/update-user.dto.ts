ba74695bd5f9c9ce4734d71b9c8ace154e28bdf3
DAVI RYAN KONUMA LIMA <0031432412006@lti.fatecso.edu.br>
2024-08-05 13:16:58 -0300

    Refiz o backend usando o nest

---
+++ backend-finance/src/user/dto/update-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsEmail, IsOptional, IsBase64 } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  password?: string;
+
+  @IsOptional()
+  @IsBase64()
+  photo?: string;
+}
