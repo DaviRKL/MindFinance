c035e386024e456515a90a04e38e4b83084cee65
DaviRKL <davirkl07@gmail.com>
2024-08-06 10:49:20 -0300

    recriei o backend de finances no nest

--- frontendmind/middleware.ts
+++ frontendmind/middleware.ts
@@ -6,8 +6,6 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
     
-
-  // Se não estiver logado e tentando acessar uma página protegida, redirecione para o login.
   if (!token && !request.nextUrl.pathname.startsWith('/login')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
