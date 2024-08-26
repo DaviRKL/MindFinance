a3f434a3483d20a7dfd623de3e1e1b2b854ea1f8
DAVI RYAN KONUMA LIMA <0031432412006@lti.fatecso.edu.br>
2024-08-26 15:29:27 -0300

    Inicio do filtro de financas

--- frontendmind/components/FinanceList.tsx
+++ frontendmind/components/FinanceList.tsx
@@ -1,10 +1,11 @@
-import React from 'react'; 
+import React, { useMemo, useState } from 'react'; 
 
 interface Finance {
     id: number;
     description: string;
     amount: number;
     type: 'INCOME' | 'EXPENSE';
+    category: string;
 }
 
 interface FinanceListProps {
@@ -14,15 +15,31 @@ interface FinanceListProps {
 } 
 
 const FinanceList: React.FC<FinanceListProps> = ({ finances, onEdit, onDelete }) => {
+  const [search, setSearch] = useState('');
+  const filtered_finances = useMemo(() => {
+    const lowerSearch = search.toLowerCase();
+    return  finances.filter((finance) =>
+     finance.category.toLowerCase().includes(lowerSearch)
+     );
+  }, [search]);
+  
   return (
     <div className="p-4 bg-white shadow-md rounded-lg">
       <h2 className="text-xl font-bold mb-4">Historico de finanças</h2>
-      {finances.map((finance) => (
+      
+      {filtered_finances.map((finance) => (
+        
         <div key={finance.id} className="flex justify-between items-center mb-2">
+          <div>
+              <input
+              type='text'
+              value={search}
+              onChange={(ev) => setSearch(ev.target.value)}/>
+          </div>
           <div>
             <p className="text-gray-700">{finance.description}</p>
             <p className="text-gray-500">${finance.amount}</p>
-            <p className="text-gray-500">{finance.type}</p>
+            <p className="text-gray-500">{finance.category}</p>
           </div>
           <div>
             <button onClick={() => onEdit(finance)} className="px-4 py-2 mr-2 bg-yellow-500 text-white rounded-md">
